486ada17ca64ee5eb7c014f6193dd38159d51799
Shamak10 <patel8sd@mail.uc.edu>
2025-06-09 14:35:27 -0400

    Add project files.

---
+++ Patel8sd_BMI_Calculator2/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Patel8sd_BMICalculator2;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        // The main page is now the AppShell itself.
+        MainPage = new AppShell();
+    }
+}
---
+++ Patel8sd_BMI_Calculator2/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Patel8sd_BMICalculator2;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        // This line now correctly calls the one, auto-generated method.
+        InitializeComponent();
+    }
+}
